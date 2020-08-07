190535b14906401f717c91884825df902aaed0cf
JurriaanD <info@jurriaan.be>
2020-08-07 22:28:56 +0200

    ♻ ️ Specify data folder instead of specific file in viewer config file

--- viewer/index.js
+++ viewer/index.js
@@ -21,34 +21,63 @@ try {
 }
 
 /* Data file administration */
-const readDataFile = () => ({
-    lastModified: fs.statSync(config.data.file_path),
-    content: fs.readFileSync(config.data.file_path, { encoding: "utf8" })
-});
 let dataFile = null;
-if (!fs.existsSync(config.data.file_path)) {
-    console.warn(`Could not find the data file at ${path.resolve(config.data.file_path)}. This may be due to a mistake in your configuration. The server will continue watching the given path.`);
-} else {
-    dataFile = readDataFile();
-}
-chokidar.watch(config.data.file_path)
-    .on("change", _path => {
-        dataFile = readDataFile();
-    })
-    .on("add", _path => {
-        if (dataFile == null) {
-            console.log("Detected the creation of the data file");
-            dataFile = readDataFile();
+let dataFilePath = null;
+let fileWatcher = chokidar.watch([]);
+fileWatcher.on("change", () => { dataFile = readDataFile(); });
+
+const getLastModifiedFile = () => {
+    let latestModifiedTime = 0;
+    let latestModifiedFilename = null;
+
+    fs.readdirSync(config.data.dataset_folder).forEach(filename => {
+        let stats = fs.statSync(path.resolve(config.data.dataset_folder, filename));
+        if (stats.isFile()) {
+            let lastModified = stats.mtimeMs;
+            if (lastModified > latestModifiedTime) {
+                latestModifiedTime = lastModified;
+                latestModifiedFilename = filename;
+            }
         }
+    });
+
+    return path.resolve(config.data.dataset_folder, latestModifiedFilename);
+};
+
+const readDataFile = () => ({
+    lastModified: fs.statSync(dataFilePath),
+    content: fs.readFileSync(dataFilePath, { encoding: "utf8" })
+});
+
+const setDataFile = newDataFilePath => {
+    fileWatcher.unwatch(dataFilePath);
+    dataFilePath = newDataFilePath;
+    dataFile = readDataFile();
+    fileWatcher.add(newDataFilePath);
+}
+
+if (!fs.existsSync(config.data.dataset_folder)) {
+    console.warn(`Could not find the folder ${path.resolve(config.data.dataset_folder)}.\r\nThe server will continue watching the path.`);
+} else {
+    dataFilePath = getLastModifiedFile();
+    if (dataFilePath == null) {
+        console.warn(`The dataset folder ${path.resolve(config.data.dataset_folder)} is empty.\r\nThe server will continue watching the folder for new files.`);
+    } else {
+        setDataFile(dataFilePath);
+    }
+}
+
+chokidar.watch(config.data.dataset_folder, { ignoreInitial: true })
+    .on("add", newFilename => {
+        // Assuming that the new file is the last modified file
+        setDataFile(path.resolve(newFilename));
     });
 
 /* Express Server */
 const app = express();
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.send('Hello World!'));
-
-app.get("/data", (req, res) => {
+app.get("/data", (_req, res) => {
     if (dataFile === null) {
         console.log("DataFile is null");
         res.status(404);
